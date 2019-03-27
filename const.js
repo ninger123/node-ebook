bb694f6e507eea9437f8f792a2d311aea77d4f91
ninger123 <1106697802@qq.com>
2019-03-27 11:26:04 +0800

    提交后端源码

---
+++ const.js
@@ -0,0 +1,33 @@
+const resUrl = 'http://192.168.43.178:8081'
+const mp3FilePath = 'C:\\ning\\resource\\mp3'
+
+const category = [
+    'Biomedicine',
+    'BusinessandManagement',
+    'ComputerScience',
+    'EarthSciences',
+    'Economics',
+    'Engineering',
+    'Education',
+    'Environment',
+    'Geography',
+    'History',
+    'Laws',
+    'LifeSciences',
+    'Literature',
+    'SocialSciences',
+    'MaterialsScience',
+    'Mathematics',
+    'MedicineAndPublicHealth',
+    'Philosophy',
+    'Physics',
+    'PoliticalScienceAndInternationalRelations',
+    'Psychology',
+    'Statistics'
+]
+
+module.exports = {
+    resUrl,
+    category,
+    mp3FilePath
+}
