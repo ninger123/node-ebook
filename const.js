a6a2ed4317e5d54f038454248490f9ce42eecbe4
ninger123 <1106697802@qq.com>
2019-03-28 23:25:48 +0800

    添加环境变量设置

--- const.js
+++ const.js
@@ -1,5 +1,14 @@
-const resUrl = 'http://192.168.43.178:8081'
-const mp3FilePath = 'C:\\ning\\resource\\mp3'
+const env = require('env')
+
+let resUrl
+let mp3FilePath
+if (env === 'dev') {
+    resUrl = 'http://192.168.43.178:8081'
+    mp3FilePath = 'C:\\ning\\resource\\mp3'
+} else if (env === 'prod') {
+    resUrl = 'http://39.96.23.9'
+    mp3FilePath = '/root/nginx/upload/mp3'
+}
 
 const category = [
     'Biomedicine',
