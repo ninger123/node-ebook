833c8e2b2156f4a8367f36886d6c136a7f1a34b1
ninger123 <1106697802@qq.com>
2019-03-29 15:38:32 +0800

    添加生产环境数据库配置

--- const.js
+++ const.js
@@ -2,12 +2,21 @@ const env = require('./env')
 
 let resUrl
 let mp3FilePath
+let dbHost
+let dbUser
+let dbPwd
 if (env === 'dev') {
     resUrl = 'http://192.168.43.178:8081'
     mp3FilePath = 'C:\\ning\\resource\\mp3'
+    dbHost = 'localhost'
+    dbUser = 'root'
+    dbPwd = '12345678'
 } else if (env === 'prod') {
     resUrl = 'http://39.96.23.9'
     mp3FilePath = '/root/nginx/upload/mp3'
+    dbHost = '39.96.23.9'
+    dbUser = 'root'
+    dbPwd = '12345678'
 }
 
 const category = [
@@ -38,5 +47,8 @@ const category = [
 module.exports = {
     resUrl,
     category,
-    mp3FilePath
+    mp3FilePath,
+    dbHost,
+    dbUser,
+    dbPwd
 }
